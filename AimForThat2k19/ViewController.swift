5603dd500e051bfc64a873662b733aab1bc8eaa6
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-03-10 17:56:43 +0100

    Upload base project

---
+++ AimForThat2k19/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AimForThat2k19
+//
+//  Created by Sebastian Ortiz Velez on 10/03/2019.
+//  Copyright © 2019 Sebastian Ortiz Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func showAlert(_ sender: Any) {
+    }
+}
+
