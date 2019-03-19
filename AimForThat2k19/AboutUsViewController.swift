0b1b1d75a72afb633c8ba0f4211d222d18b253e2
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-03-19 16:55:33 +0100

    Add instructions view controller

---
+++ AimForThat2k19/AboutUsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutUsViewController.swift
+//  AimForThat2k19
+//
+//  Created by Sebastian Ortiz Velez on 19/03/2019.
+//  Copyright © 2019 Sebastian Ortiz Velez. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
